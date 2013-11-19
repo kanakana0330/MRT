ec068ccb8483710597626bc7c2d9245d24325e43
kanakoAoki <kanako@kanakoAoki-no-MacBook-Air.local>
2013-11-19 22:27:29 +0900

    added config.rb

---
+++ mrt_config_develop.rb
@@ -0,0 +1,9 @@
+http_path = "./"
+css_dir = "css_debug"
+sass_dir = "res/sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :expanded
+line_comments = false
+sass_options = {:debug_info => true}
---
+++ mrt_config_production.rb
@@ -0,0 +1,8 @@
+http_path = "./"
+css_dir = "css"
+sass_dir = "res/sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :compressed
+line_comments = false
