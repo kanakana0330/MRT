061f5b176a2a6eda09ea026e6fa78abfd9d0830e
kanakana0330 <kanakana0330@gmail.com>
2014-07-24 14:51:07 +0900

    orientationchange対応してみたい

--- public/script/base.js
+++ public/script/base.js
@@ -1,25 +1,39 @@
 $(function(){
   var $_mainCont = $('.mainCont');
+
   var fadeSpeed = 1500;
   var intarval = 6000;
-  var w = $(window).width();
-  var breakPoint = 850;
 
-  var init = function(){
-    $_mainCont.children('.topImg').css({opacity:'0'});
-    $_mainCont.children('.topImg').eq(0).stop().animate({opacity:'1',zIndex:'2'},fadeSpeed);
-  }
+  var breakPoint = 850;
+  var flg = 0;
 
   var changeImages = function(){
+    $_mainCont.children('.topImg').css({opacity:'0'});
+    $_mainCont.children('.topImg').eq(0).stop().animate({opacity:'1',zIndex:'2'},fadeSpeed);
+
     setInterval(function(){
       $_mainCont.children('.topImg').eq(0).animate({opacity:'0'},fadeSpeed).next('.topImg').animate({opacity:'1'},fadeSpeed).end().appendTo($_mainCont);
     },intarval);
   }
 
-  if(w > breakPoint){
-    init();
-    changeImages();
+  var judgeBreakPoint = function(){
+    var w = $(window).width();
+
+    if(w > breakPoint){
+      changeImages();
+      flg = 1;
+    } else {
+      if(flg == 1){
+        location.reload();
+      }
+      flg = 0;
+    }
   }
+
+  $(window).on('orientationchange', judgeBreakPoint);
+
+  judgeBreakPoint();
+
   
 });
 
