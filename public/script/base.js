29724051aedd636d901c9c344fce399c4bcf45ba
kanakana0330 <kanakana0330@gmail.com>
2014-07-24 10:07:06 +0900

    modified base.js

--- public/script/base.js
+++ public/script/base.js
@@ -3,12 +3,21 @@ $(function(){
   var fadeSpeed = 1500;
   var intarval = 6000;
 
-  $_mainCont.children('.topImg').css({opacity:'0'});
-  $_mainCont.children('.topImg').eq(0).stop().animate({opacity:'1',zIndex:'2'},fadeSpeed);
+  var init = function(){
+    $_mainCont.children('.topImg').css({opacity:'0'});
+    $_mainCont.children('.topImg').eq(0).stop().animate({opacity:'1',zIndex:'2'},fadeSpeed);
+  }
 
-  setInterval(function(){
-    $_mainCont.children('.topImg').eq(0).animate({opacity:'0'},fadeSpeed).next('.topImg').animate({opacity:'1'},fadeSpeed).end().appendTo($_mainCont);
-  },intarval);
+  var changeImages = function(){
+    setInterval(function(){
+      $_mainCont.children('.topImg').eq(0).animate({opacity:'0'},fadeSpeed).next('.topImg').animate({opacity:'1'},fadeSpeed).end().appendTo($_mainCont);
+    },intarval);
+  }
+
+
+
+  init();
+  changeImages();
 
 });
 
